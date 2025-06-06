20dc4f0e6640fc4592c2de4bf435782a28174911
Raquel Vuicik <raquel.vuicik@artemidas.net>
2025-06-06 17:43:21 -0300

    componente tarefa 1

--- projeto-01-tarefas/tarefas/src/App.vue
+++ projeto-01-tarefas/tarefas/src/App.vue
@@ -2,7 +2,8 @@
 	<div id="app">
 		<h1>Tarefas</h1>
 		<NewTask @taskAdded="addTask"/>
-		<TaskGrid :tasks="tasks" />
+		<TaskGrid @taskDeleted="deleteTask"
+			:tasks="tasks" />
 	</div>
 </template>
 
@@ -31,6 +32,9 @@ export default {
 					pending: task.pending || true
 				})
 			}
+		},
+		deleteTask(i) {
+			this.tasks.splice(i, 1)
 		}
 	}
 
