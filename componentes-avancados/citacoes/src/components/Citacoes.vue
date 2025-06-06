740741b8a7024b6581f66ccce3941f61ffd06efc
Raquel Vuicik <raquel.vuicik@artemidas.net>
2025-06-06 11:26:55 -0300

    passando conteudo no corpo do componente

--- componentes-avancados/citacoes/src/components/Citacoes.vue
+++ componentes-avancados/citacoes/src/components/Citacoes.vue
@@ -4,12 +4,15 @@
             <button @click="numero--">&lt;</button>
             <button @click="numero++">&gt;</button>
         </span>
-        {{ numero }} {{ indice }}
+        <citacao :texto="citacoes[indice].texto" />
     </div>
 </template>
 
 <script>
+import Citacao from './Citacao.vue'
+
 export default {
+    components: { Citacao },
     data() {
         return {
             numero: 0,
