338ce4c3e5bcfd8a672c91ea9b5c9d276b2a3ade
Raquel Vuicik <raquel.vuicik@artemidas.net>
2025-06-06 11:47:57 -0300

    usando slot padrao

--- componentes-avancados/citacoes/src/components/Citacoes.vue
+++ componentes-avancados/citacoes/src/components/Citacoes.vue
@@ -6,7 +6,7 @@
         </span>
         <citacao>
             <h1 slot="autor">{{ citacoes[indice].autor }}</h1>
-            <p slot="texto">{{ citacoes[indice].texto }}</p>
+            <p>{{ citacoes[indice].texto }}</p>
             <h6 slot="fonte">{{ citacoes[indice].fonte }}</h6>
         </citacao>
     </div>
